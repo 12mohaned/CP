248d9beb843891cd54a14f95c6604819d5689022
12mohaned <30902228+12mohaned@users.noreply.github.com>
2019-01-07 13:57:44 +0200

    Update Beautiful_year.java

--- Beautiful_year.java
+++ Beautiful_year.java
@@ -1,43 +1,32 @@
-import java.io.*;
-import java.util.*;
-public class Problem_set_2
+public static int Beautifulyear(int year){
+boolean f = true;
+while(f){
+year = year +1;
+String s = year+"";
+int i = 0;
+int y1 = s.charAt(i); 
+int y2 = s.charAt(i+1);
+int y3 = s.charAt(i+2);
+int y4 = s.charAt(i+3);
+for(i =0; i < s.length();i++){
+if(y1 != y2 & y1 != y3 & y1 != y4){
+if(y2 != y3 & y2 != y4){
+if(y3 != y4){
+f = false;
+}
+}
+}
+}
+year = Integer.parseInt(s);
+}
+return year;
+}
+public static void main(String[]args) throws IOException
 {
-public static void main(String[]args)
-{
-Scanner sc = new Scanner(System.in);
-int counter = 0;
-int n = sc.nextInt();
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+StringBuilder s = new StringBuilder();
 PrintWriter r = new PrintWriter(System.out);
-int []a = new int[n*2];
-int i =0;
-int j =0;
-String s ="";
-while(n > j){
-int x = sc.nextInt();
-int y = sc.nextInt();
-a[i] = x;
-a[i+1] = y;
-i = i+2;
-j++;
-if(x != y){
-s = "rated";
-}
-}
-for(i =0; i < n*2 & s!="rated" ;i++){
-for(j =0 ;j < n*2;j++){
-if( j > i){
-if(i% 2== 0 & j % 2== 0){
-if(a[i] <a[j]){
-s = "unrated";
-}
-}
-}
-}
-}
-if(s != "rated" & s!= "unrated"){
-s = "maybe";
-}
-r.println(s);
+int x = Integer.parseInt(br.readLine());
+r.println(Beautifulyear(x));
 r.flush();
-}
 }
